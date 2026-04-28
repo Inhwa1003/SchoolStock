7263ac40e0b02fb14b3f23f3a247d0dbe9d161e9
이인화 <KOSTA@DESKTOP-GA5J7BB>
2026-04-28 15:48:15 +0900

    Libraries 추가, FrontControllerServlet 추가

---
+++ src/com/school/stockGame/servlet/FrontControllerServlet.java
@@ -0,0 +1,20 @@
+package com.school.stockGame.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class FrontControllerServlet
+ */
+@WebServlet("/controller")
+public class FrontControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public FrontControllerServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
