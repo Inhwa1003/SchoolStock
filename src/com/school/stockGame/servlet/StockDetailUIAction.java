8e4f2e1264be12c53e50c7992b678191752aea6b
최동석 <choidongseok95@gamil.com>
2026-04-29 15:13:17 +0900

    주식 기본 정보 업무 구현 완료

--- src/com/school/stockGame/servlet/StockDetailUIAction.java
+++ src/com/school/stockGame/servlet/StockDetailUIAction.java
@@ -15,11 +15,13 @@ public class StockDetailUIAction implements Action {
 		
 		try {
 			StockDetailDAO stockDetailDAO = new StockDetailDAO();
+			
 			// 주식 기본 정보 가져오기 - 나중에 주식목록 페이지에서 주식 아이디 request에 넘어온 값으로 조회
-			Map<String, Object>m = stockDetailDAO.getStockInfo(2);
+			Map<String, Object>m = stockDetailDAO.getStockInfo(1);
 			request.setAttribute("stockName", m.get("name"));
 			request.setAttribute("stockContent", m.get("content"));
-			stockDetailDAO.ConnClose();
+			
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
