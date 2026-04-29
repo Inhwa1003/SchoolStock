b3275fde005943c9b6b839958d5af64f0eede3c5
인화 <iinhwa016@gmail.com>
2026-04-29 14:11:47 +0900

    StockListQuery수정 - StockDetailQuery를 재사용하기로함.

--- src/com/school/stockGame/dao/StockListDAO.java
+++ src/com/school/stockGame/dao/StockListDAO.java
@@ -11,7 +11,8 @@ import com.school.stockGame.query.StockListQuery;
 public class StockListDAO {
 
     public StockListDAO() {}
-
+    
+    // 1. 주식명 조회
     public List<String> getStockNameList() {
         List<String> stockNameList = new ArrayList<String>();
 
@@ -19,6 +20,7 @@ public class StockListDAO {
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
+        
         try {
             conn = DBCP.getConnection();
 
@@ -43,7 +45,14 @@ public class StockListDAO {
                 e.printStackTrace();
             }
         }
-
         return stockNameList;
     }
+    
+    // 2. 주식 현재가격 조회  
+    // 3. 주식 이전가격, 전장마감가 조회
+    // 4. 주식 이전가 대비 조회
+    // 5. 주식 등락률 조회
+    // 6. 주식 목록 화면용 전체 정보 조회
+    
+    
 }
--- src/com/school/stockGame/query/StockListQuery.java
+++ src/com/school/stockGame/query/StockListQuery.java
@@ -3,8 +3,16 @@ package com.school.stockGame.query;
 public interface StockListQuery {
 	// 주식 목록 페이지 정보 조회
 	
-	// 주식명 조회
+	// 1. 주식명 조회
 	String STOCK_NAME_LIST_SQL="SELECT name FROM stocks";
 	
+	// 아래 쿼리는 StockDetail에 있는 거 가져오기로 함.
+	// 2. 주식 현재가격 조회
+
+	// 3. 주식 이전가격, 전장마감가 조회
+
+	// 4. 주식 이전가 대비 조회
+
+	// 5. 주식 등락률 조회
 
 }
