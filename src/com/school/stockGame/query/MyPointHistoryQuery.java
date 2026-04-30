333e3fcc491cdaa193f3444fcf79e11724e1ccfd
gitdong95 <choidongseok95@gmail.com>
2026-04-30 14:45:31 +0900

    주식 상세 페이지 비동기 구동 확인 (#26)
    * test
    
    * test
    
    * dao패키지 DBCP추가
    
    * dao패키지 StockDetailDAO추가
    
    * dao패키지 CouponDAO추가
    
    * query패키지 stockDetailQuery인터페이스 추가
    
    * query패키지 CouponQuery인터페이스 추가
    
    * vo패키지 stockVO빼고 7개 추가
    
    * Junit4 라이브러리 추가 test패키지 StockDetailDAOTest추가
    
    * view폴더에 stockDetail.jsp 추가
    
    * WebContent밑에 css폴더 생성 css폴더에 stock_style.css 추가
    
    * 톰캣 8.0추가
    
    * servlet패키지 Action인터페이스, ActionFactory, FrontControllerServlet 추가
    
    * ActionFactory 로그인 관련 삭제
    
    * servlet패키지 StockDetailUIAction 추가
    
    * /controller <- 접속시 나올 시작 페이지 추가
    
    * StockDetailDAO에 ConnClose() 추가
    
    * WEB-INF/lib 폴더 추가 그 밑에 gson, jatl, ojdbc 라이브러리 추가
    
    * 주식상세정보 페이지 주식기본정보조회 업무 완료
    
    * DAO conn 관련 수정 (기존 action객체에서 conn.close() 한던거 dao에서 하게 변경)
    
    * StockDetailDAO 기본생성자 추가
    
    * 주식 기본 정보 업무 구현 완료
    
    * 주식 이전가격, 등락률, 주식 이전가 대비, 주식 현재가 업무 4개 동기처리로 구현 완료
    
    * 종목상세 페이지 등록된 주문현황 기능 비동기 구현
    
    * 주식상세 업무 4개 추가 (주문요청 완료, 주식발행정보조회, 주식발행정보개수변경, 주문요청상태정보 변경)
    
    * ㄹㅇㄴ
    
    ---------
    
    Co-authored-by: 최동석 <choidongseok95@gamil.com>

--- src/com/school/stockGame/query/MyPointHistoryQuery.java
+++ src/com/school/stockGame/query/MyPointHistoryQuery.java
@@ -4,5 +4,5 @@ public interface MyPointHistoryQuery {
 	String MY_POINT_HISTORY_SQL="SELECT cp.purchase_date, cp.price, cp.name, gp.content, gp.point, gp.get_date, o.content, o.price, o.amount, o.order_date, s.name, t.transaction_date FROM COUPON_PURCHASE cp, COUPONS c, GET_POINT gp, ORDERS o, STOCKS s, TRANSACTION t, STUDENTS WHERE STUDENTS.student_id='abc'";
 	
 	
-
+ㅇ
 }
