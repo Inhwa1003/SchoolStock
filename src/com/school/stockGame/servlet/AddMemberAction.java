c8ee40e272b22d04f9ae819e88063fb8dd5b3768
천사무엘 <mfmfvlrm@naver.com>
2026-05-01 17:33:05 +0900

    AddMemberAction 추가

---
+++ src/com/school/stockGame/servlet/AddMemberAction.java
@@ -0,0 +1,36 @@
+package com.school.stockGame.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.school.stockGame.dao.MemberDAO;
+
+public class AddMemberAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) 
+			throws ServletException, IOException {
+		String url="AddMember.jsp";
+		try {
+			MemberDAO dao=new MemberDAO();
+			if(dao.addMember(request.getParameter("studentId"),
+					request.getParameter("password"),
+					request.getParameter("name"),
+					Integer.parseInt(request.getParameter("grade")),
+					request.getParameter("className"),
+					Integer.parseInt(request.getParameter("classNumber"))))
+				url="Login.jsp";
+			else
+				request.setAttribute("errorMessage", "회원가입실패");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		return url;
+	}
+
+}
