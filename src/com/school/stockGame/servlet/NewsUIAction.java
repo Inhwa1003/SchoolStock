5cb33f810b9750f5499aecf1b7dd275d1ab8f1a5
천사무엘 <mfmfvlrm@naver.com>
2026-05-01 17:43:03 +0900

    NewsUIAction 추가

---
+++ src/com/school/stockGame/servlet/NewsUIAction.java
@@ -0,0 +1,19 @@
+package com.school.stockGame.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.school.stockGame.dao.NewsDAO;
+
+public class NewsUIAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		NewsDAO dao = new NewsDAO();
+		request.setAttribute("newsList", dao.getNewsList());
+		return "NewsList.jsp";
+	}
+
+}
