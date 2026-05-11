65b773d6f179933b286810fe4ffce914b707163f
ehdtm <ehdtm@D>
2026-05-12 00:16:49 +0900

    setStudentAssets Junit 테스트 완료

--- src/test/com/school/stockGame/dao/CouponDAOMybatisTest.java
+++ src/test/com/school/stockGame/dao/CouponDAOMybatisTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import com.school.stockGame.dao.mybatis.DBCPMybatis;
 import com.school.stockGame.vo.CouponPurchaseVO;
+import com.school.stockGame.vo.CouponVO;
 
 public class CouponDAOMybatisTest {
 	
@@ -17,6 +18,19 @@ public class CouponDAOMybatisTest {
 		boolean flag = false;
 		try {
 			flag = session.insert("couponMapper.setPurchaseRecord", new CouponPurchaseVO(100, "간식 교환권", 1, "abc", 1)) == 1;
+			assertTrue(flag);
+		} finally {
+			session.rollback();
+			session.close();
+		}
+	}
+	
+	@Test
+	public void setStudentAssetsTest(){
+		SqlSession session = DBCPMybatis.getSqlSessionFactory().openSession();
+		boolean flag = false;
+		try {
+			flag = session.update("couponMapper.setStudentAssets", new CouponVO(100, "abc")) == 1;
 			assertTrue(flag);
 		} finally {
 			session.rollback();
