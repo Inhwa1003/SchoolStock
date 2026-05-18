b32adbe8b1eeaaa058e763aa4398ff0d8a53d48c
skfkfkvlrm <skmfmfvlrm@gmail.com>
2026-05-19 01:24:41 +0900

    오해를 발생시킨 주석 수정
    StockDetailDAOMybatis에서 필요한 메서드를 재사용으로 수정했습니다

--- src/com/school/stockGame/dao/StockListDAOInterface.java
+++ src/com/school/stockGame/dao/StockListDAOInterface.java
@@ -6,7 +6,7 @@ import com.school.stockGame.vo.StockVO;
 
 public interface StockListDAOInterface {
 	// 현재 기존 파일들을 최대한 그대로 사용가능하게 작성하는 중입니다
-	// 따라서 추후 스프링 부트로 리팩토링 할 때 부족한 메서드들을 선언하는 것이 맞다고 판단했습니다.
+	// 따라서 StockDetailDAOMybatis.java에서 필요한 메서드를 재사용합니다
 	// 1. 주식명 조회
 	public List<StockVO> getStockNameList();
 	// 2. 주식 현재가격 조회  
